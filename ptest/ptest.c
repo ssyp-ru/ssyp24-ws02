0fdac17c441bd54bde4a00b3570aeef50197d6ba
ssyp <a.ishikaev@gmail.com>
2024-07-19 10:29:01 +0700

    ptest v2

--- ptest/ptest.c
+++ ptest/ptest.c
@@ -5,37 +5,37 @@
 #include <string.h>
 #include "ptest.h"
 
-ptest_ctx test_data[256];
-int test_data_len = 0;
 void ptest_run(void (*f)(ptest_ctx *ctx)) {
 	srand(time(0));
 	ptest_ctx ctx;
 	for (int i = 0; i < 100; i++) {
+        ctx.elems_len = 0;
 		f(&ctx);
-        test_data_len = 0;
+        ctx.tests ++;
 	}
 }
 
 void ptest_fail(ptest_ctx *ctx) {
 	// TODO: print generated data of failed runv ds
-    for(int i = 0; i < test_data_len; i ++) {
-        printf("gen %s: %d\n", test_data[i].name, test_data[i].value);
+    for(int i = 0; i < ctx->elems_len; i ++) {
+        printf("gen %s: %d\n", ctx->elems[i].name, ctx->elems[i].value);
     }
+    printf("Tests: %d\n", ctx->tests + 1);
     assert(0);
 }
 
 int ptest_int(ptest_ctx *ctx, char* name) {
 	int val = rand();
-	strcpy(test_data[test_data_len].name, name);
-	test_data[test_data_len].value = val;
-    test_data_len ++;
+	strcpy(ctx->elems[ctx->elems_len].name, name);
+	ctx->elems[ctx->elems_len].value = val;
+    ctx->elems_len ++;
 	return val;
 }
 
 int ptest_int_range(ptest_ctx *ctx, int from, int to, char *name) {
 	int val = rand() % (to - from) + from;
-	strcpy(test_data[test_data_len].name, name);
-	test_data[test_data_len].value = val;
-    test_data_len ++;
+	strcpy(ctx->elems[ctx->elems_len].name, name);
+	ctx->elems[ctx->elems_len].value = val;
+    ctx->elems_len ++;
 	return val;
 }
